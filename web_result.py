a5523262548dfc04a5426a95c51f4f61996968d8
Jacky Lee <hklee310@gmail.com>
2018-01-19 18:20:53 -0800

    heroku demo hack

--- web_result.py
+++ web_result.py
@@ -16,6 +16,12 @@ from dal import db
 app = Flask(__name__)
 
 @app.route("/")
+def hello_world(data=None):
+    print("hello logs")
+
+    return "hello world"
+
+@app.route("/home")
 def hello(data=None):
     print("hello")
 
@@ -48,5 +54,7 @@ def zzz(data=None):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run()
+    if __name__ == '__main__':
+        # Bind to PORT if defined, otherwise default to 5000.
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
