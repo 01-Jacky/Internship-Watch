7a5de972d1382650fc6cfce559abe6f7c2bb8d9f
Jacky Lee <hklee310@gmail.com>
2018-01-19 18:02:22 -0800

    heroku requirements

--- web_result.py
+++ web_result.py
@@ -9,14 +9,16 @@ from flask import render_template
 import pickle
 from os import listdir
 from os.path import isfile, join
+import os
 
 from dal import db
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello(data=None):
-
+    print("Main route called")
     # Load the dictionary back from the pickle file.
     # jobs = pickle.load(open("data_dump/jobs_2017-12-24_0132.p", "rb"))
     # latest_file = ''
@@ -27,7 +29,9 @@ def hello(data=None):
     # jobs = pickle.load(open(latest_file, "rb"))
     # print(latest_file)
 
+    print("doing db.get_jobs()...")
     jobs = db.get_jobs()
+    print("Got em...")
 
     # discard >30 days
     fresh_jobs = [job for job in jobs]
@@ -42,3 +46,7 @@ def hello(data=None):
 def zzz(data=None):
     return render_template('example.html')
 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run()
